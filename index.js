46fc95a32460f7f652c063650c9450d743bc9760
Seda Ünal <sedaylmz06@outlook.com>
2022-11-03 17:53:14 +0300

    find method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+  //...
+];
+function superbowlWin(record) {
+  if (record === undefined) {
+    return "undefined value";
+  }
+  return record.result === "W";
+}
+console.log(record.find(superbowlWin));
